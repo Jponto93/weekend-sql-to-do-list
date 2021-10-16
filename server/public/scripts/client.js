5a77192539b6450280eac29e0f8be382a00d1e28
Jordan Ponto <jponto93@gmail.com>
2021-10-16 13:43:07 -0500

    client side GET route works with server side now

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,18 @@ $(onReady);
 
 function onReady () {
     console.log('JQ');
-    
+    getTasks();
 } // end onReady
+
+function getTasks () {
+    $.ajax({
+        method: 'GET',
+        url: '/tasks',
+    }).then(function (response) {
+        console.log(response);
+        // renderTasks();
+        
+    }).catch(function (error) {
+        console.log('Error in client GET', error);
+    })
+} // end getTasks
