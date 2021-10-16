5a28fa45aaa71d2d11918960b77e16aaad58a668
Jordan Ponto <jponto93@gmail.com>
2021-10-16 14:07:02 -0500

    render tasks to table is set up with complete/delete btn

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -13,9 +13,34 @@ function getTasks () {
         url: '/tasks',
     }).then(function (response) {
         console.log(response);
-        // renderTasks();
+        renderTasks(response);
         
     }).catch(function (error) {
         console.log('Error in client GET', error);
     })
 } // end getTasks
+
+function renderTasks (response) {
+    let el = $('#viewTasks');
+    el.empty();
+
+    for (let i = 0; i < response.length; i++){
+        let id = response[i].id
+        if (!response[i].complete){
+            completeBtn = `<button class="completeBtn">Task Complete</button>`;
+        }
+        let entry = $(`
+        <tr data-id"${id}">
+            <td>${response[i].task}</td>
+            <td>${response[i].description}</td>
+            <td>${response[i].urgency}</td>
+            <td>${response[i].complete}</td>
+            <td>${completeBtn}</td>
+            <td><button class="deleteBtn">Delete</button</td>
+        </tr>`);
+
+        el.append(entry);
+        
+    } // end for
+
+} // end renderTasks
