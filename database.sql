fc901ed4f606a4c3cccf22c48724a92b104e75a2
Jordan Ponto <jponto93@gmail.com>
2021-10-16 12:37:52 -0500

    added form to html, updated database structure and updated database.sql

--- database.sql
+++ database.sql
@@ -1,12 +1,14 @@
 CREATE TABLE "tasks" (
 "id" SERIAL,
+"urgency" VARCHAR (6),
 "task" VARCHAR (50),
 "description" VARCHAR (300),
-"task_complete" BOOLEAN
+"complete" BOOLEAN
 );
 
-INSERT INTO "tasks" ("task", "description", "task_complete")
+INSERT INTO "tasks" ("task", "urgency", "description", "complete")
 VALUES
-('Grocery shop', 'get eggs and bacon', 'false'),
-('Laundry', 'dark load', 'false'),
-('Clean', 'kitchen and bathroom', 'false');
+('Grocery shop', 'High', 'get eggs and bacon', 'false'),
+('Laundry', 'Medium', 'dark load', 'false'),
+('Clean', 'Low', 'kitchen and bathroom', 'false');
+
